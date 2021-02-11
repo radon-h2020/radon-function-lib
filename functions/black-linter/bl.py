60f0e661c1b8f364fa9c56f0a7b787bbe78e0067
naesheim <hg.nesheim@gmail.com>
2021-02-11 15:02:43 +0100

    added tree travers

--- functions/black-linter/bl.py
+++
@@ -1,8 +0,0 @@
-##
-## NB testing purpose
-###
-from black import reformat_many, FileMode, WriteBack, Report
-from pathlib import Path
-
-reformat_many({Path(".")},False,WriteBack.DIFF,FileMode(),Report(False,True,False,True))
-# result = format_file_in_place(Path("black_linter.py"),False,FileMode(),WriteBack.DIFF)
--- functions/black-linter/black_linter.py
+++ functions/black-linter/black_linter.py
@@ -1,12 +1,11 @@
 import os
-import subprocess
 import json
 from zipfile import ZipFile
 from black import format_file_in_place, FileMode, WriteBack
 from pathlib import Path
+import pprint
 
 LOCAL_PATH = "/tmp/repo"
-
 
 def handler(event, context):
     # retrieve body and email to verify risk factor
@@ -14,12 +13,9 @@ def handler(event, context):
         params = json.loads(event["body"])
     else:
         params = event
-
     try:
         git_repo = params["git_repo"]
-
-
-
+        git_branch = params["git_branch"] if "git_branch" in params else "master"
     except KeyError:
         return {
             "body": json.dumps({"message": "git_repo is not provided"}),
@@ -28,14 +24,20 @@ def handler(event, context):
         }
 
     try:
-        download_command = f"wget {git_repo}/archive/master.zip -P {LOCAL_PATH}"
+        download_command = f"wget {git_repo}/archive/{git_branch}.zip -P {LOCAL_PATH}"
         os.system(download_command)
 
-        with ZipFile("/tmp/repo/master.zip", "r") as zipObj:
-            zipObj.extractall("")
+        with ZipFile(f"{LOCAL_PATH}/{git_branch}.zip", "r") as zipObj:
+            zipObj.extractall(f"{LOCAL_PATH}")
 
-        lint_command = f"black {LOCAL_PATH} --diff"
-        result = subprocess.check_output(lint_command, shell=True)
+        # lint_command = f"black {LOCAL_PATH} --diff"
+        result = {}
+        for root, dirs, files in os.walk(LOCAL_PATH):
+            for file in files:
+                if file.endswith(".py"): 
+                    result[file] = format_file_in_place(Path(f"{root}/{file}"),False,FileMode(),WriteBack.DIFF)
+
+        # result = subprocess.check_outpu1t(lint_command, shell=True)
         return {
             "body": result,
             "headers": {"Content-Type": "application/json"},
@@ -47,3 +49,14 @@ def handler(event, context):
             "headers": {"Content-Type": "application/json"},
             "statusCode": 200,
         }
+
+if __name__ == "__main__":
+    test_event = {}
+    # test cases
+    #  test_json_file = "tests/test.json"
+    test_json_file = "tests/test.json"
+    with open(test_json_file) as test_json:
+        test_event = json.load(test_json)
+    test_context = {}
+    test_res = handler(test_event, test_context)
+    print(test_res)
