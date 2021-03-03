6eac9f8915407d856a275fd63c2a648d82324ce4
naesheim <hg.nesheim@gmail.com>
2021-03-03 10:50:26 +0100

    finishing touch

--- functions/weirdaal/keycheck.py
+++ functions/weirdaal/keycheck.py
@@ -1,10 +1,7 @@
 import json
 import boto3
 import botocore
-import pprint
 import sys
-
-pp = pprint.PrettyPrinter(indent=5, width=80)
 
 region = 'us-east-1'
 regions = ['us-east-1']
@@ -13,13 +10,13 @@ def check_root_account(session):
     '''
     Do various checks to see if the account has root or elevated IAM privs
     '''
-    client = boto3.client('iam', region_name=region)
-
     try:
+        client = session.client('iam', region_name=region)
+        
         data = {}
         acct_summary = client.get_account_summary()
         if acct_summary:
-            data[f"client {session.get_credentials().access_key}"] = "Root Key!!! This is either a root key or has IAM access"
+            data[f"User {session.get_credentials().access_key}"] = "Root Key!!! This is either a root key or has IAM access"
 
         client_list = client.list_users()
         for user in client_list['Users']:
@@ -34,19 +31,10 @@ def check_root_account(session):
                 if e.response['Error']['Code'] == 'NoSuchEntity':
                     data[user['UserName']] = "likely doesnt have console access"
                 else:
-                    print("Unexpected error: {}" .format(e))
-        
+                    return e
         return data
-
     except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == 'InvalidClientTokenId':
-            sys.exit("The AWS KEY IS INVALID. Exiting")
-        if e.response['Error']['Code'] == 'AccessDenied':
-            print('{} : Is NOT a root key' .format(AWS_ACCESS_KEY_ID))
-        elif e.response['Error']['Code'] == 'SubscriptionRequiredException':
-            print('{} : Has permissions but isnt signed up for service - usually means you have a root account' .format(AWS_ACCESS_KEY_ID))
-        else:
-            print("Unexpected error: {}" .format(e))
+        return e.response['Error']['Message']        
 
 
 def handler(event, context):
@@ -57,28 +45,19 @@ def handler(event, context):
     try:
         AWS_SECRET_KEY_ID= params['aws_secret_key_id']
         AWS_SECRET_ACCESS_KEY = params['aws_secret_access_key']
+
+        session = boto3.Session(AWS_SECRET_KEY_ID, AWS_SECRET_ACCESS_KEY)
+        data = check_root_account(session)
+        return {
+            "body": data,
+            "headers": {"Content-Type": "application/json"},
+            "statusCode": 200,
+        }
     except KeyError:
         return {
             "body": json.dumps({"message": "keys not passed correctly"}),
             "headers": {"Content-Type": "application/json"},
             "statusCode": 401,
-        }
-    try:
-
-
-        session = boto3.Session(AWS_SECRET_KEY_ID, AWS_SECRET_ACCESS_KEY)
-
-        return {
-            "body": {"message": check_root_account(session)},
-            "headers": {"Content-Type": "application/json"},
-            "statusCode": 200,
-        }
-    except Exception as e:
-        message = e.message if hasattr(e,'message') else e
-        return {
-            "body": {"message": message},
-            "headers": {"Content-Type": "application/json"},
-            "statusCode": 200,
         }
 
 if __name__ == "__main__":
